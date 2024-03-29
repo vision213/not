72a39f0aa58ea271ad170fc6da02d93834c649d5
vision213 <137607604+vision213@users.noreply.github.com>
2024-03-30 02:29:05 +0530

    Update CounterfeitDrugDetection.sol

--- CounterfeitDrugDetection.sol
+++ CounterfeitDrugDetection.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CounterfeitDrugDetection {
+contract counterfeitDrugDetection {
     // Structure to represent a drug
     struct Drug {
         uint256 batchNumber;
